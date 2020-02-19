4b52d7010fb8aa8dce0a76f2dbf017ea5dc15fb0
root <root@ip-172-31-3-138.ap-south-1.compute.internal>
2020-02-19 06:48:38 +0000

    mycookbooks

---
+++ apache-cookbook/recipes/sample-recipe.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook:: apache-cookbook
+# Recipe:: sample-recipe
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+file '/robofile' do
+  content "New latest Attributes
+  HOSTNAME: #{node['hostname']}
+  IPADDRESS: #{node['ipaddress']}
+  CPU: #{node['cpu']['0']['mhz']}
+  MEMORY: #{node['memory']['total']}"
+  owner 'root'
+  group 'root'
+  action :create
+end
+
